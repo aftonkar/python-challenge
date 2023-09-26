0812cbde635b7185d569ee322c40915877bd27d9
aftonkar <aftonkar@gmail.com>
2023-09-25 23:11:52 -0700

    Adding the calculations and output for total and average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,25 +13,40 @@ print("Financial Analysis\n--------------------------")
 with open(budget_csv) as budget_file:
     budget_reader = csv.reader(budget_file, delimiter=",")
 
-
+    data = list(budget_reader)
+    print(len(data))
     #Count the number of months by counting the rows, skipping the header row
-    num_months = -1
-    for row in budget_reader:
-        num_months += 1
+    num_months = len(data) - 1
+    
+    #Calculate the total amount of Profits/Losses
+    net_total = 0
+
+    #
+    for i in range(1, len(data)):
+        num = data[i]
+        net_total += int(num[1])
+    
+    #Display the average of changes in "Profit/Losses"
+    counter = 0
+    total = 0
+
+    for i in range(1, len(data) - 1):
+        num = data[i]
+        next_num = data[i + 1]
+        total += float(next_num[1]) - float(num[1])
+
+        counter = i
+
+    average_change = total/counter
 
     #Display the total number of months    
     print(f"Total Months: {num_months}")
 
-    #Total the amount of "Profit/Losses" -- I think I need to organize the "column" into a list so something like .zip? but the reverse of that
-    net_total = sum()
-    #for row in budget_reader:
-    #    net_total += float(row.split()[1])
-        
-
     #Display the net total amount of "Profit/Losses"
     print(f"Total: ${net_total}")
 
-#Display the average of changes in "Profit/Losses"
+    #Display the average change
+    print(f"Average Change: ${average_change:.2f}")
 
 
 #Output the greatest increase in profits (date and amount) over the entire period
